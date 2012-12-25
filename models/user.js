f8d3725674715169194b65c59a1d7d84662990d5
Riku <riku@seppa.la>
2012-12-25 15:54:52 +0200

    Changed the model with geo

--- models/user.js
+++ models/user.js
@@ -6,7 +6,8 @@ mongoose.connect(config.development.dbUrl);
 var userSchema = new mongoose.Schema({
 	fbId: String,
 	name: String,
-	friends_list: [Number]
+	friends_list: [Number],
+	geo: {type: [Number], index: '2d'}
 });
 
 module.exports = mongoose.model('User', userSchema);
