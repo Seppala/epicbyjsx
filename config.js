91e1233bfec2f1d733c4f043f8f441bd4bc8a600
Riku <riku@seppa.la>
2012-12-25 14:17:41 +0200

    Hoperfully, made facebook work on both dev and locally...

--- config.js
+++ config.js
@@ -2,12 +2,29 @@ var dbUrl = process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL || 
   'mongodb://localhost/test';
 
+var appId;
+var appSecret;
+var fbUrl;
+
+if (typeof(NODE_ENV) !== 'undefined') 
+	{
+		appId = '513729218671638';
+		appSecret = '7e5317d985ebb4a9222b21477ffaf9f3';
+		fbUrl = 'http://agile-plateau-5423.herokuapp.com/';
+	}
+else 
+	{
+		appId = "217545681715200";
+		appSecret = "713d928ae56d98762a74ea61e096252d";
+		fbUrl = 'http://localhost:5000/';
+	}
+
 module.exports = {
 	development: {
 		fb: {
-			appId: "217545681715200",
-			appSecret: "713d928ae56d98762a74ea61e096252d",
-			url: "http://localhost:5000/" 
+			appId: appId,
+			appSecret: appSecret,
+			url: fbUrl 
 		},
 		dbUrl: dbUrl
 	}
