2a27685c5c723ce6f383bc13ff6b3f77c3faf515
Riku <riku@seppa.la>
2013-03-30 14:08:26 +0200

    Strange behavior with the API GET (/api/friends) and PUT (/api/users/:fbid)
    Currently, when the server is started, and you log in as user, the PUT request to api/users/:fbid doesn't work, but the GET request to api/friends works.
    
    Then, if you restart the server, WITHOUT refreshing the page, the PUT request works, but the GET request gets response 302 "moved temporarily"
    
    Somehow the requests must be changing, but they look the same?

--- app.js
+++ app.js
@@ -245,10 +245,11 @@ app.get('/api/users', function (req, res){
   });
 });
 
-//OK, now this put request works. 
-app.put( '/api/users/:id', function( request, response ) {
+//OK, now this put request works. Nope, just sometimes, argh!
+app.put( '/api/users/:fbId', function( request, response ) {
     console.log( 'Updating user ' + request.body.name + ' fbid:' + request.body.fbId);
-    return UserModel.findOne({ fbId: request.body.fbId }, function( err, user ) {
+    
+	return UserModel.findOne({ fbId: request.body.fbId }, function( err, user ) {
         console.log('user that was found :' + user)
 		user.upfo = request.body.upfo;
 
