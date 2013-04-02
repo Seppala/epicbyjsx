c0f108227917068c4dcebc46745736a0d8795f58
Riku <riku@seppa.la>
2013-04-02 08:58:32 +0300

    Added tests folder

---
+++ tests/fb_users.js
@@ -0,0 +1,14 @@
+var config = require('../config');
+var FbTestUrl = 'https://graph.facebook.com/' + fb.appId +'/test-users?'
+
+var url
+https://graph.facebook.com/APP_ID/accounts/test-users?
+  installed=true
+  &name=FULL_NAME
+  &locale=en_US
+  &permissions=read_stream
+  &method=post
+  &access_token=APP_ACCESS_TOKEN
+
+// Creates the fake fb users that we want.
+var createFbUsers = 
