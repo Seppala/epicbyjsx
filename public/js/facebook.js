ded6e3810363fbef65cc430a32449846dd7235ae
Riku <riku@seppa.la>
2013-04-13 18:28:05 +0300

    Enabled user to share to facebook when he's upfo

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -1,3 +1,4 @@
+/* SETUP *********/
 
 var surl = document.URL.split('/')
 console.log('document URL:' + surl[2])
@@ -6,10 +7,12 @@ console.log('document URL:' + surl[2])
 
 if (surl[2] === 'localhost:5000') {
 	appID = '217545681715200';
+	link = 'http://localhost:5000';
 }
 
 else {
 	appID = '513729218671638';
+	link = 'http://piazzo.co';
 }
 
 FB.init({
@@ -17,10 +20,12 @@ FB.init({
         frictionlessRequests: true
       });
 
+/* INVITES *************/
+
 function sendFBRequest() {
   FB.ui({
     method: 'apprequests',
-    message: 'invites you to learn how to make your mobile web app social',
+    message: 'Hey, join Piazzo and well be able to meet more often! Its an easy way find friends for lunch, dinner or any escapade right now right here.',
   }, 
   function(response) {
     console.log('sendRequest response: ', response);
@@ -29,11 +34,29 @@ function sendFBRequest() {
 
 function sendRequestToRecipients(fbId) {
   FB.ui({method: 'apprequests',
-    message: 'My Great Request',
+    message: 'Hey, join Piazzo and well be able to meet more often! Its an easy way find friends for lunch, dinner or any escapade right now right here.',
     to: fbId
   }, requestCallback);
 }
 
 function requestCallback(response) {
 	console.log('user sent request from the list');
+}
+
+/*POSTS****************/
+
+function publishFBStory() {
+  FB.ui({
+    method: 'feed',
+	//message: msg,
+    name: 'Piazzo - meet more friends',
+    caption: 'Piazzo is a simple way to figure out who to call when you want to do something',
+    description: 'Ever want to find someone for lunch but dont know who to call? Want to have more people over for a pre-party?',
+    link: link,
+    picture: 'http://www.facebookmobileweb.com/getting-started/img/facebook_icon_large.png'
+  }, 
+  function(response) {
+    console.log('publishStory response: ', response);
+  });
+  return false;
 }
