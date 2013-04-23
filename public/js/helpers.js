29f1a41f8c74466ddcefa3846cb69a5e4e5e98ea
Finn Pauls <derfinn@gmail.com>
2013-04-23 23:16:56 +0300

    Helper function for geolocation from browser added

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -9,3 +9,16 @@ function serverSettimer(time) {
 function serverDestroytimer() {
 	$.post("/api/destroytimer/");
 };
+
+var getLocation = function() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			next(false, position.coords.latitude, position.coords.longitude);
+		}, 
+		function(error) {
+			next(typeof msg == 'string' ? msg : "Error with geolocation.");
+		});
+	} else {
+		next('Geolocation is not supported by the browser');
+	}
+}
