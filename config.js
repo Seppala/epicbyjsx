e58f8afde5ffa5066cc22941b8eba858b87fe9d6
Riku <riku@seppa.la>
2013-06-07 16:52:26 +0300

    FbUrl was wrong in settings...

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ else if (process.env.NODE_ENV == 'productioneu')
 	{
 		appId = '607217585963588';
 		appSecret = 'cbbc35fe7ba759e270c45118e8799d3a';
-		fbURL = 'http://piazzoeu.herokuapp.com';
+		fbUrl = 'http://piazzoeu.herokuapp.com';
 		upfoTen = 600000;
 		upfoHour = 3600000;
 	}
@@ -40,7 +40,7 @@ module.exports = {
 	fb: {
 		appId: appId,
 		appSecret: appSecret,
-		url: fbUrl 
+		url: fbUrl
 	},
 	dbUrl: dbUrl,
 	FBURL: FBURL,
