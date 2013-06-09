a87a60d99ca34c2e9999e1d9c95018c3a3a381ae
Riku <riku@seppa.la>
2013-06-09 18:15:53 +0300

    Changed the url for piazzo-us to piazzo.herokuapp.com

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV == 'production')
 	{
 		appId = '513729218671638';
 		appSecret = '7e5317d985ebb4a9222b21477ffaf9f3';
-		fbUrl = 'http://piazzo.co/';
+		fbUrl = 'http://piazzo.herokuapp.com/';
 		upfoTen = 600000;
 		upfoHour = 3600000;
 	}
--- routes/routehelpers.js
+++ routes/routehelpers.js
@@ -1,6 +1,11 @@
 
 exports.ensureAuthenticated = function(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
+  if (req.isAuthenticated()) 
+	{ return next(); 
+	}
+  else {
+	console.log('ensureAuthenticated failed');
+	} 
   res.sendfile('views/index.html');
 }
 
