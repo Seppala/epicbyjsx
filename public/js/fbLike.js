29409bde426113faef39796d1f035c202c686d32
Riku <riku@seppa.la>
2013-06-09 16:40:46 +0300

    More appID updates on public..

--- public/js/fbLike.js
+++ public/js/fbLike.js
@@ -12,7 +12,7 @@ if (place === 'dev') {
 		var js, fjs = d.getElementsByTagName(s)[0];
 		if (d.getElementById(id)) return;
 		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=513729218671638";
+		js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=607217585963588";
 		fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
 }
