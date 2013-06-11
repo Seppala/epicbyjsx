73c7ccbfe69fc5b53fde38e0545cee6ae5f81ea4
Riku <riku@seppa.la>
2013-06-11 22:42:05 +0300

    Added uservoice.js

---
+++ public/js/uservoice.js
@@ -0,0 +1,13 @@
+
+<!-- A tab to launch the Classic Widget -->
+UserVoice = window.UserVoice || [];
+UserVoice.push(['showTab', 'classic_widget', {
+  mode: 'feedback',
+  primary_color: '#8e8e8e',
+  link_color: '#2795b6',
+  forum_id: 210472,
+  tab_label: 'Feedback',
+  tab_color: '#cfd9db',
+  tab_position: 'bottom-left',
+  tab_inverted: false
+}]);
